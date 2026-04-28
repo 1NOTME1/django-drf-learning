f1099999ac7ab6fbefb192a48410a6401bb54366
m.kwasniewski <m.kwasniewski@gpm-vindexus.pl>
2026-04-28 09:49:15 +0200

    feat: added user serializer validation

--- users/serializer.py
+++ users/serializer.py
@@ -4,4 +4,14 @@ from .models import UserProfile
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
-        fields = ["id" ,"name", "age", "is_active"]
+        fields = ["id", "name", "age", "is_active"]
+    def validate_name(self, value):
+        name = value.strip()
+        if not name:
+            raise serializers.ValidationError("Name cannot be empty")
+        return name
+    
+    def validate_age(self, value):
+        if value < 0 or value > 120:
+            raise serializers.ValidationError("Age must be between 0 and 120")
+        return value
